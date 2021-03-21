f0f2a0b14a914713be1db0766166cb7b3a85732b
wnsdnwnsdn11 <wnsdnwnsdn11@gmail.com>
2021-03-22 02:55:11 +0900

    remove hello.java and added MenuManager.java

---
+++ src/MenuManager.java
@@ -0,0 +1,12 @@
+
+public class MenuManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Contacts");
+		System.out.println("2. Delete Contacts");
+		System.out.println("3. Edit Contacts");
+		System.out.println("4. View Contacts");
+		System.out.println("5. Exit");
+	}
+
+}
